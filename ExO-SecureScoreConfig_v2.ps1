efe3804ee9269ceb0a72ecc8cbb76b8091a1a78b
Ard84 <86302561+Ard84@users.noreply.github.com>
2022-02-06 23:15:52 +0100

    Add files via upload

--- ExO-SecureScoreConfig_v2.ps1
+++ ExO-SecureScoreConfig_v2.ps1
@@ -1,5 +1,6 @@
 #region declarations
-$Script:tenantDomain = Get-AutomationVariable -Name "SCM_Domain" #please endter the defualt domain of the tenant the managed identity belongs to.
+$Domain = Get-AutomationVariable -Name "SCM_Domain"
+$Script:tenantDomain = Get-AutomationVariable -Name $Domain #please endter the defualt domain of the tenant the managed identity belongs to.
 #endregion declarations
 
 #region functions
@@ -19,7 +20,7 @@ function connectEXOAsMSI ($OAuthCredential) {
         Write-Verbose "Creating new EXOPSSession..." -Verbose
         try {
             $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/PowerShell-LiveId?BasicAuthToOAuthConversion=true&email=SystemMailbox%7bbb558c35-97f1-4cb9-8ff7-d53741dc928c%7d%40$tenantDomain" -Credential $OAuthCredential -Authentication Basic -AllowRedirection
-            $null = Import-PSSession $Session -DisableNameChecking -CommandName "*Get-Msol*","*Get-Transport*","*-Mailbox*","*Get-AutomationVar*","*Get-SafeAttachmentPolicy*","*Get-SafeLinkPolicy*","*New-Transport*","*New-Safe*","*Get-AcceptedDomain*" -AllowClobber
+            $null = Import-PSSession $Session -DisableNameChecking -CommandName "*-HostedOutboundSpamFilterPolicy*","*New-Transport*","*New-Safe*","*-Mailbox*","*-Sharing*","*Set-AdminAuditLog*" -AllowClobber
             Write-Verbose "New EXOPSSession established!" -Verbose
         } catch {
             Write-Error $_
@@ -33,38 +34,21 @@ function connectEXOAsMSI ($OAuthCredential) {
 #region execute
 $null = Connect-AzAccount -Identity
 $null = Connect-ExchangeOnline -Identity
-$null = Connect-MsolService -Identity
 connectEXOAsMSI -OAuthCredential (makeMSIOAuthCred)
 
 #Do some exchange scripting here!
 #Automation Variables
-Connect-MsolService
-$GlobalAdminsRoleGroup = Get-MsolRole | ? { $_.Name -eq "Company Administrator" }
-$NotifyOutboundSpamRecipients = (Get-MsolRoleMember -RoleObjectId $GlobalAdminsRoleGroup.ObjectId -MemberObjectTypes User -All).EmailAddress
-$NotifyOutboundSpamRecipients = (Get-MsolCompanyInformation).TechnicalNotificationEmails
-$Clientrules = Get-TransportRule | Select Name
-$AtpMailbox = Get-Mailbox
-$Domains = (Get-AcceptedDomain | Where-Object {$_.Default -eq $true}) 
-$Domain = Get-AutomationVariable -Name "SCM_Domain"
-$SafeAttachmentPolicies = Get-SafeAttachmentPolicy
-$SafeLinksPolicies = Get-SafeLinksPolicy
 
 
 #Enable Outbound Spam Filtering Rules
-#Automation Variables
 $OutboundSpamFilteringRules_Enabled = Get-AutomationVariable -Name "OutboundSpamFilteringRules_Enabled"
-
-
-if($OutboundSpamFilteringRules_Enabled -Like "Yes") {
+if($OutboundSpamFilteringRules_Enabled -Like "true") {
     Get-HostedOutboundSpamFilterPolicy | Set-HostedOutboundSpamFilterPolicy -NotifyOutboundSpam $true -NotifyOutboundSpamRecipients $NotifyOutboundSpamRecipients
 }
 
 # Enable Client Forwarding Block Transport Mail Flow Rule:
-#Automation Variables
 $ClientForwardBlockRules_Enabled = Get-AutomationVariable -Name "ClientForwardBlockRules_Enabled" 
-
-
-if($ClientForwardBlockRules_Enabled -Like "Yes") {
+if($ClientForwardBlockRules_Enabled -Like "true") {
     
     if ($Clientrules.Name -Like "Client Rules Forwarding Block") {
         Write-Host '***Client Rules Forwarding Block Already Exists'
@@ -79,12 +63,12 @@ if($ClientForwardBlockRules_Enabled -Like "Yes") {
     }
 }
 
-#Automation Variables
+#EnableAntipishing Rules
 $SafeAttachmentRules_Enabled = Get-AutomationVariable -Name "SafeAttachmentRules_Enabled"
 $SafeLinkRules_Enabled = Get-AutomationVariable -Name "SafeLinkRules_Enabled"
 
 
-if($SafeAttachmentRules_Enabled -Like "Yes" -Or $SafeLinkRules_Enabled -Like "Yes") {
+if($SafeAttachmentRules_Enabled -Like "true" -Or $SafeLinkRules_Enabled -Like "true") {
 
     # Create a Redirected Mailbox for mail that gets flagged by the Safe Attachment policy to be delivered to.
     if ($AtpMailbox.Name -Like "ATPRedirectedMessages") {
@@ -96,13 +80,13 @@ if($SafeAttachmentRules_Enabled -Like "Yes" -Or $SafeLinkRules_Enabled -Like "Ye
         Add-MailboxPermission -Identity "ATPRedirectedMessages@$($Domain)" -AutoMapping $false -InheritanceType All -User $cred.UserName -AccessRights FullAccess
 
         # Create a new Safe Attachment policy.
-        if($SafeAttachmentRules_Enabled -Like "Yes") {
+        if($SafeAttachmentRules_Enabled -Like "true") {
             New-SafeAttachmentPolicy -Name 'Default Safe Attachment Policy' -AdminDisplayName 'Default Safe Attachment Policy' -Action Replace -Redirect $True -RedirectAddress "ATPRedirectedMessages@$($Domain)" -Enable $True
             New-SafeAttachmentRule -Name 'Default Safe Attachment Rule' -RecipientDomainIs $Domain -SafeAttachmentPolicy 'Default Safe Attachment Policy' -Enabled $True
         }
 
         # Create a new Safe Links policy.
-        if($SafeLinkRules_Enabled -Like "Yes") {
+        if($SafeLinkRules_Enabled -Like "true") {
             New-SafeLinksPolicy -Name Default -AdminDisplayName Default -TrackClicks $true -IsEnabled $true -AllowClickThrough $false -ScanUrls $true
             New-SafeLinksRule -Name Default -RecipientDomainIs $Domain -SafeLinksPolicy Default -Enabled $true
         }
@@ -112,24 +96,20 @@ if($SafeAttachmentRules_Enabled -Like "Yes" -Or $SafeLinkRules_Enabled -Like "Ye
 # Disallow anonymous Calendar Sharing: Free/Busy And Disallow anonymous Calendar Detail Sharing
 #Automation Variables
 $AnonymousCalendarSharingRules_Enabled = Get-AutomationVariable -Name "AnonymousCalendarSharingRules_Enabled"
-
-
-if($AnonymousCalendarSharingRules_Enabled -Like "Yes") {
-    Get-SharingPolicy | Set-SharingPolicy -Domains @{ Remove = "Anonymous:CalendarSharingFreeBusyReviewer"; Add = "Anonymous:0" }
-    Get-SharingPolicy | Set-SharingPolicy -Enabled $False
+if($AnonymousCalendarSharingRules_Enabled -Like "true") {
+    New-SharingPolicy -Name "Anonymous" -Domains 'Anonymous: CalendarSharingFreeBusySimple' -Enabled $false
+        foreach($user in Get-Mailbox -RecipientTypeDetails UserMailbox) {Set-SharingPolicy -Mailbox $user -Name "Anonymous"}
+    Get-SharingPolicy "Anonymous"| Format-list
 }
 
 
 # Set mailbox auditing on all mailboxes
 #Automation Variables
 $MailboxAuditingRules_Enabled = Get-AutomationVariable -Name "MailboxAuditingRules_Enabled"
-
-
-if($MailboxAuditingRules_Enabled -Like "Yes") {
+if($MailboxAuditingRules_Enabled -Like "true") {
     Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox" -or RecipientTypeDetails -eq "SharedMailbox" -or RecipientTypeDetails -eq "RoomMailbox" -or RecipientTypeDetails -eq "DiscoveryMailbox"} | Set-Mailbox -AuditEnabled $true -AuditLogAgeLimit 730 -AuditAdmin Update, MoveToDeletedItems, SoftDelete, HardDelete, SendAs, SendOnBehalf, Create, UpdateFolderPermission -AuditDelegate Update, SoftDelete, HardDelete, SendAs, Create, UpdateFolderPermissions, MoveToDeletedItems, SendOnBehalf -AuditOwner UpdateFolderPermission, MailboxLogin, Create, SoftDelete, HardDelete, Update, MoveToDeletedItems 
     Write-Host -ForegroundColor Green "Set mailbox auditing on all mailboxes."
     # Enable audit data recording
-    Enable-OrganizationCustomization -ea silentlycontinue -wa silentlycontinue
     Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true -ea silentlycontinue -wa silentlycontinue
     Write-Host -ForegroundColor Green "Enabled Audit Data Recording"
 }

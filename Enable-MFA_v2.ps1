290c00a8270c9ce42ee0acb185848e3774b68707
Ard84 <86302561+Ard84@users.noreply.github.com>
2022-02-09 09:53:49 +0100

    Update Enable-MFA_v2.ps1

--- Enable-MFA_v2.ps1
+++ Enable-MFA_v2.ps1
@@ -4,7 +4,6 @@ $MFA_Enabled = Get-AutomationVariable -Name "MFA_Enabled"
 #Enable MFA Policies
 if($MFA_Enabled -Like "Yes") {
   $cred = Get-AutomationPSCredential -Name "MSOL"
-  Connect-AzureAD -Credential $cred
   Connect-MsolService -Credential $cred
 
   #Enable MFA
@@ -14,17 +13,15 @@ if($MFA_Enabled -Like "Yes") {
   $multiFactor.RememberDevicesNotIssuedBefore = (Get-Date) 
   $multiFactorOff = @()
   $domains = Get-MsolDomain
-  $secureScoreUser = Get-AutomationVariable -Name "User"
+ 
 
   #For all users turn MFA on
   Get-MsolUser | ForEach-Object {
       Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $multiFactor
   }
 
-  #Turn off MFA for the SecureScore user. This is done explicitly in case SecureScore user is not in BreakGlass group yet
-  Set-MsolUser -UserPrincipalName $secureScoreUser -StrongAuthenticationRequirements $multiFactorOff 
 
-  #If there is a group called "SecureScoreBreakGlass" then exclude the additional users (i.e. Turn MFA Off for them)
+  #If there is a group called "BreakGlass" then exclude the additional users (i.e. Turn MFA Off for them)
   if ((Get-MsolGroup -SearchString "BreakGlass" | Measure-Object).Count -gt 0) {
     Get-MsolGroupMember -GroupObjectId (Get-MsolGroup -SearchString "BreakGlass").ObjectId | ForEach-Object {
       Set-MsolUser -UserPrincipalName $_.EmailAddress -StrongAuthenticationRequirements $multiFactorOff
